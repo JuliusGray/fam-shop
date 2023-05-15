ea88a446fb523b2fd7a928ff7c28e71d133b78e1
JupickGosling <gizbrekht.y@mail.ru>
2023-05-15 12:10:06 +0300

    services fix

--- src/assets/data/serviceData.js
+++ src/assets/data/serviceData.js
@@ -1,19 +1,19 @@
 const serviceData = [
   {
     icon: "ri-truck-line",
-    title: "Бесплатная доставка",
+    title: "Бесплатная\nдоставка",
     subtitle: "Lorem ipsum dolor sit amet.",
     bg: "#fdefe6",
   },
   {
     icon: "ri-secure-payment-line",
-    title: "Безопасная оплата",
+    title: "Безопасная\nоплата",
     subtitle: "Lorem ipsum dolor sit amet.",
     bg: "#e2f2b2",
   },
   {
     icon: "ri-exchange-dollar-line",
-    title: "Гарантия возврата",
+    title: "Гарантия\nвозврата",
     subtitle: "Lorem ipsum dolor sit amet.",
     bg: "#d6e5fb",
   },
