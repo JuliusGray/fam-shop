a72b6bc00472ec6199cb4baa9c387273dcaadc90
JupickGosling <gizbrekht.y@mail.ru>
2023-05-31 00:13:46 +0300

    1

--- src/components/UI/ProductCard.jsx
+++ src/components/UI/ProductCard.jsx
@@ -30,7 +30,8 @@ const ProductCard = ({ item }) => {
     <Col lg="3" md="4" className="mb-2">
       <div className="product__item">
         <div className="product__img">
-          <motion.img whileHover={{ scale: 0.9 }} src={item.imgUrl} alt="" />
+        <Link to={`/shop/${item.id}`}><motion.img whileHover={{ scale: 0.9 }} src={item.imgUrl} alt="" /></Link>
+          {/* <motion.img whileHover={{ scale: 0.9 }} src={item.imgUrl} alt="" /> */}
         </div>
         <div className="p-2 product__info">
           <h3 className="product__name">
